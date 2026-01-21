bd75aaf938d6568a66952bd0c93057f8a887c2f3
V.M. SAI KUAMR <mahendarsairam@gmail.com>
2026-01-21 09:07:17 +0530

    Add medicine recommendations for common ailments

---
+++ medicine.py
@@ -0,0 +1,6 @@
+medicine_dict = {
+    "dengue": "Paracetamol (Avoid aspirin, consult doctor immediately)",
+    "fever": "Paracetamol",
+    "headache": "Ibuprofen",
+    "healthy": "No medicine required"
+}
